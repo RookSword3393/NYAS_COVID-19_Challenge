05dac622ce148327ce72eb67a6d04afcf002b2a1
Jinen Setpal <jinens8@gmail.com>
2020-04-20 16:13:28 +0530

    Update commit.sh

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -3,6 +3,6 @@
 git config user.email "jinens8@gmail.com"
 git config user.name "bot"
 
-git add currentreport.csv
+git add ../../currentreport.csv
 git commit -m "automated update" 
 git push origin $1
