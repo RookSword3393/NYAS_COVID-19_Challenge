355b231320bce79f9320bb6bebec22457a4fe147
Jinen Setpal <jinens8@gmail.com>
2020-06-23 19:57:17 +0530

    commit date integrated

--- .github/workflows/commit.sh
+++ .github/workflows/commit.sh
@@ -4,5 +4,5 @@ git config user.email "jinens8@gmail.com"
 git config user.name "bot"
 
 git add ../../currentreport.csv
-git commit -m "automated update" 
+git commit -m "automated update for "$2 
 git push origin $1
